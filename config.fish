26b265e1447ee05a29b8a527e44f80d9d8e9c922
Anton-Latukha <anton.latukha@gmail.com>
2019-04-01 11:21:37 +0300

    enable vi shell mode

--- config.fish
+++ config.fish
@@ -3,3 +3,6 @@ function l --description 'alias ls -lahvF --group-directories-first --time-style
   ls -lahvF --group-directories-first --time-style=iso --color $argv;
 
 end
+
+# Crazy: Enable vi key bindings
+set -g fish_key_bindings fish_vi_key_bindings
