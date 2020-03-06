43ca2c29a2874d75c866bcec8c2ac87d28e058fd
Anton-Latukha <anton.latukha@gmail.com>
2020-03-07 01:51:27 +0200

    conf: enhancd: add default filtering tool

--- config.fish
+++ config.fish
@@ -116,3 +116,7 @@ set -gu 'async_prompt_inherit_variables' 'all'
 #### Offload UI functions to async process
 set -gu 'async_prompt_functions' 'fish_prompt rWakatime done git_is_repo git_branch_name git_is_touched git_ahead prompt_pwd fish_right_prompt'
 
+## b4b4r07/enhancd configuration
+#### Filter directories with
+set -gu 'ENHANCD_FILTER' 'fzf'
+
