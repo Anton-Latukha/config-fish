04e6d877a0eb4682c43eba97d075436f2110d0ae
Anton-Latukha <anton.latukha@gmail.com>
2020-11-11 20:38:03 +0200

    M  config.fish

--- config.fish
+++ config.fish
@@ -131,3 +131,6 @@ set -gu 'ENHANCD_FILTER' 'fzf'
 #### display notifications if a command longer then
 set -gu '__done_min_cmd_duration' '10000'
 set -gu '__done_exclude' '(git (?!push|pull|clone)|emacs|e |uEmacs|dEmacs)'  # default: all git commands, except push and pull. accepts a regex.
+
+## Modify the PATH
+set -gu fish_user_paths $HOME/.cabal/bin $HOME/.ghcup/bin
