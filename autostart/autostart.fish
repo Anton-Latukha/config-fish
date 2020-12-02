ba8a406d85e79a149d82958a0fa0c1ca9dc7e66b
Anton-Latukha <anton.latukha@gmail.com>
2020-12-02 03:26:42 +0200

    M  autostart/autostart.fish

--- autostart/autostart.fish
+++ autostart/autostart.fish
@@ -4,17 +4,16 @@
 systemctl --user import-environment PATH S O H HH HB NP N XDG_RUNTIME_DIR SSH_AUTH_SOCK
 systemctl --user start emacs
 
-# Telegram
-run-app-on-desktop telegram-desktop 2
-
 # Firefox
 run-maximized-app-on-desktop firefox 2
+
+# Telegram
+run-app-on-desktop telegram-desktop 2
 
 # Chromium
 run-maximized-app-on-desktop chromium 2
 sleep 1
 move-current-window-to-desktop 2
-
 
 # Emacs clients
 
@@ -22,15 +21,21 @@ move-current-window-to-desktop 2
 emacsclient --eval '(daemonp)'
 ## Run and kill first frame (to workaround the Spacemacs specific first buffer and load org-agenda on second launch)
 emacsclient -c --eval "(run-with-timer 2 nil (delete-frame))"
+emacsclient --eval '(daemonp)'
+emacsclient -c --eval "(run-with-timer 2 nil (delete-frame))"
 
 run-maximized-app-on-desktop 'emacsclient -c' 0
+
+wmctrl -a 'Telegram'
+
+wmctrl -a 'Firefox'
 
 run-maximized-app-on-desktop 'emacsclient -c' 1
 
 run-app-on-desktop 'emacsclient -c' 2
 
 # Switch to Desktop 0
-wmctrl -s 0
+move-to-desktop 0
 
 # Launch KeePass
 run-app-on-desktop keepass 0
