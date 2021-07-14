4a227c8eec25e602cdc5f94d1845ad031a1065b2
Anton-Latukha <anton.latukha@gmail.com>
2021-07-14 17:36:21 +0300

    config: add debug flag

--- config.fish
+++ config.fish
@@ -1,5 +1,8 @@
 ## Using heavy quotation to speed-up execution process
 
+# Debug:
+# set fish_trace on
+
 function 'l' --description 'alias ls -lahvF --group-directories-first --time-style=iso --color'
 
   ls -lahvF --group-directories-first --time-style='long-iso' --color='auto' $argv;
